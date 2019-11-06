0c82c1f521f0def4033cc092c62bb75e7c3cffb0
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2019-11-06 12:33:31 -0500

    Added Audio Mixer
    Finsihed the Audio Mixer

---
+++ Assets/Scripts/Audio_Mix.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class Audio_Mix : MonoBehaviour
+{
+    public AudioMixer MasterAudio;
+    public void SetSfxLvl(float sfxLvl)
+    {
+        MasterAudio.SetFloat("sfxVol",sfxLvl); 
+    }
+    public void SetMusicLvl(float MusicLvl)
+    {
+        MasterAudio.SetFloat("musicVol", MusicLvl);
+    }
+
+}
