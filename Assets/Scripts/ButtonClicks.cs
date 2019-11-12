acd2bf5f7bf89f798daf607a7118cc6f15824e84
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2019-11-12 13:26:57 -0500

    Menu's Done
    The Menu's done in my test scene. i dont know if the menu images were done so i jusy used a place holder img

---
+++ Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonClicks : MonoBehaviour
+{
+    public GameObject MenuIMG;
+    public GameObject SettingsIMG;
+    public GameObject MenuBTN;
+    void Start()
+    {
+        MenuIMG.SetActive(false);
+        SettingsIMG.SetActive(false);
+        MenuBTN.SetActive(true);
+    }
+    public void Main_Menu_BTN()
+    {
+        MenuIMG.SetActive(true);
+        SettingsIMG.SetActive(false);
+        MenuBTN.SetActive(false);
+    }
+    public void quitGame()
+    {
+        MenuIMG.SetActive(false);
+        SettingsIMG.SetActive(false);
+        MenuBTN.SetActive(true);
+        Application.Quit();
+    }
+    public void MenuClose()
+    {
+        MenuIMG.SetActive(false);
+        SettingsIMG.SetActive(false);
+        MenuBTN.SetActive(true);
+    }
+    public void Settings()
+    {
+        SettingsIMG.SetActive(true);
+        MenuIMG.SetActive(true);
+    }
+    public void RTNGame()
+    {
+        MenuIMG.SetActive(false);
+        SettingsIMG.SetActive(false);
+        MenuBTN.SetActive(true);
+    }
+}
