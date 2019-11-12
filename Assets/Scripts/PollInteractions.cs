75a7c73328df2b9905dac38c66a34f31ac49f2e2
Kyle <arow411@gmail.com>
2019-11-12 17:09:30 -0500

    Add Geo Tracking

---
+++ Assets/Scripts/PollInteractions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollInteractions : MonoBehaviour
+{
+    public UIManager uiManager;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        if (Camera.main == null) return;
+        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out var hit, 1000, 1 << 9))
+        {
+            uiManager.ShowPoll(null);
+            //hit.collider.gameObject
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject inGameUI;
+    public GameObject pauseMenu;
+    public GameObject pollUI;
+
+    public void ReturnToGame()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        inGameUI.SetActive(true);
+    }
+
+    public void ShowPoll(PollStop pollStop)
+    {
+        pollUI.SetActive(true);
+        inGameUI.SetActive(false);
+    }
+
+    public void ShowOptions()
+    {
+        pauseMenu.SetActive(true);
+        inGameUI.SetActive(false);
+    }
+}
