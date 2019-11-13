0bdeae2e820df6edd4d2529d5f4ced41c2bfb7da
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2019-11-13 17:05:19 -0500

    Got the Speed
    Attach the FInding_Players_Speed script to the player and it will find the speed based no the distance the player moved.

---
+++ Assets/Scripts/Finding_Players_Speed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Finding_Players_Speed : MonoBehaviour
+{
+    Vector3 prevFramPos = Vector3.zero;
+    public float speed = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveperframe = Vector3.Distance(prevFramPos, transform.position);
+
+        speed = moveperframe / Time.deltaTime;
+
+        prevFramPos = transform.position;
+    }
+}
---
+++ Assets/Scripts/For_Brandons_Test_Not_The_Actual_Movement_Code.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class For_Brandons_Test_Not_The_Actual_Movement_Code : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = -transform.forward * speed;
+        }
+    }
+}
