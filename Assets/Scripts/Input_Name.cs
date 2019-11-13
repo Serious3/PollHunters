956960508fd0b787ce66d7266b60dce6f7a8d19f
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2019-11-13 15:23:45 -0500

    Input Jeff
    Input scene

--- Assets/Scripts/Input_Name.cs
+++ Assets/Scripts/Input_Name.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Input_Name : MonoBehaviour
 {
+    public InputField field;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class Input_Name : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InputText();
+    }
+    public void InputText()
+    {
+        string text;
+
+        text = field.text;
+        if(text == "Jeff" && Input.GetKey(KeyCode.KeypadEnter)|| text == "jeff" && Input.GetKey(KeyCode.KeypadEnter) 
+            || text == "JEFF" && Input.GetKey(KeyCode.KeypadEnter))
+        {
+            
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Jeff");
+        }
+
     }
 }
